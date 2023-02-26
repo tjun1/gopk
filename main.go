c9cdc8207dcd8aad0345c4c358c0564057ded4f6
tjun1 <gaiant.baba@gmail.com>
2023-02-27 02:13:30 +0900

    コメント文を英文に変える。

--- main.go
+++ main.go
@@ -23,10 +23,13 @@ func handleError(err error) {
 	}
 }
 
-// 引数にファイルを受け取る
-// ファイルの構造:
+// Accepts a filename as an argument.
+// Structure of the document file:
+// <URL>\s\|\s"Description" or empty line.
+//
+// for example:
 // https://github.com/japan-clojurians/curriculum | japan-clojurians/curriculum
-// <URL>\s\|\s"Description" か 空行 の2種類で構成されている
+
 func main() {
 	var (
 		f = flag.String("f", "", "File name with URL listed.")
@@ -45,8 +48,6 @@ func main() {
 
 	scanner := bufio.NewScanner(fp)
 	re := regexp.MustCompile("^http(.*)://(.*)")
-	// https://www.google.com/search?q=
-
 	googleDriveURLRE := regexp.MustCompile(`^https://drive.google.com/(.*)`)
 	googleDocslURLRE := regexp.MustCompile(`^https://docs.google.com/(.*)`)
 	gmailURLRE := regexp.MustCompile(`^https://mail.google.com/`)
@@ -55,15 +56,15 @@ func main() {
 	googleMapURLRE := regexp.MustCompile(`^https://www.google.co.jp/maps(.*)`)
 	urls := []string{}
 
-	// 登録対象とするURLのスライスを作る
+	// Create a slice of the URL
 	for scanner.Scan() {
 		line := scanner.Text()
-		// 行を分割してURL部分だけ抽出する
-		seperated_line := strings.Split(line, " |")
-		// 空行を間引いて http:// もしくは https:// から始まる行だけ抽出する
-		fs := re.FindString(seperated_line[0])
+		// Split the line and take out only the URL part.
+		url_part := strings.Split(line, " |")[0]
+		// Extract only lines beginning with http:// or https://
+		fs := re.FindString(url_part)
 		if len(fs) != 0 {
-			// Google系サービスは除外する
+			// Exclude Google services.
 			is_googleSearchURL := googleSearchURLRE.MatchString(fs)
 			is_googleDriveURL := googleDriveURLRE.MatchString(fs)
 			is_googleDocsURL := googleDocslURLRE.MatchString(fs)
@@ -73,10 +74,12 @@ func main() {
 			if is_googleSearchURL || is_googleDriveURL || is_googleDocsURL || is_gmailURL || is_googleCalendarURL || is_googleMapURL {
 				continue
 			}
+			// Adding a URL to a slice.
 			urls = append(urls, fs)
 		}
 	}
-	// urls の要素が0個、つまり登録するものがなければ終了する
+
+	// If there are zero elements in the slice, i.e., there are none to register, then the program is terminated.
 	if len(urls) == 0 {
 		fmt.Println("No more than one URL was found.")
 		os.Exit(1)
